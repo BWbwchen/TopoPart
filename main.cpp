6f42195232c231260e8cb672b04c7c0a29c513e5
BWbwchen <tim.chenbw@gmail.com>
2023-01-09 23:16:06 +0800

    [docs   ]: add license
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/* Copyright (C) 2023 Bo-Wei Chen<time.chenbw@gmail.com> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 #include <fstream>
 #include <iostream>
 
